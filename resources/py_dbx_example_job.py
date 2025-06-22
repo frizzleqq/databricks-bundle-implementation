40b9572d882dd03f85cc471caa561ddb380c6677
frizzleqq <flo.fritz@protonmail.com>
2025-06-22 17:13:25 +0200

    constants and ruff addition

--- resources/py_dbx_example_job.py
+++ resources/py_dbx_example_job.py
@@ -2,16 +2,18 @@ import json
 
 from databricks.bundles.core import Variable, variables
 from databricks.bundles.jobs import (
-    Environment,
     Job,
-    JobEnvironment,
-    PeriodicTriggerConfiguration,
     PythonWheelTask,
+    RunIf,
     Task,
-    TriggerSettings,
 )
 
 from dbx_example import tasks
+
+from .constants import DAILY_TRIGGER, DEFAULT_ENVIRONMENT, ENTRY_POINT, PACKAGE_NAME
+
+# Workflow Settings
+JOB_NAME = "py_dbx_example_job"
 
 
 @variables
@@ -19,45 +21,41 @@ class Variables:
     catalog_name: Variable[str]
 
 
-default_environment = JobEnvironment(
-    environment_key="default", spec=Environment(client="2", dependencies=["./dist/*.whl"])
-)
-
 py_dbx_example_job = Job(
-    name="py_dbx_example_job",
-    trigger=TriggerSettings(periodic=PeriodicTriggerConfiguration(interval=1, unit="DAYS")),
+    name=JOB_NAME,
+    trigger=DAILY_TRIGGER,
     tasks=[
         Task(
             task_key="bronze_taxi",
             python_wheel_task=PythonWheelTask(
-                package_name="dbx_example",
-                entry_point="dbx-example",
+                package_name=PACKAGE_NAME,
+                entry_point=ENTRY_POINT,
                 parameters=[
                     tasks.BronzeTaxiTask.get_class_name(),
                     "--catalog",
                     Variables.catalog_name,
                 ],
             ),
-            environment_key="default",
+            environment_key=DEFAULT_ENVIRONMENT.environment_key,
         ),
         Task(
             task_key="bronze_accuweather",
             python_wheel_task=PythonWheelTask(
-                package_name="dbx_example",
-                entry_point="dbx-example",
+                package_name=PACKAGE_NAME,
+                entry_point=ENTRY_POINT,
                 parameters=[
                     tasks.BronzeAccuweatherTask.get_class_name(),
                     "--catalog",
                     Variables.catalog_name,
                 ],
             ),
-            environment_key="default",
+            environment_key=DEFAULT_ENVIRONMENT.environment_key,
         ),
         Task(
             task_key="silver_nyctaxi_aggregate",
             python_wheel_task=PythonWheelTask(
-                package_name="dbx_example",
-                entry_point="dbx-example",
+                package_name=PACKAGE_NAME,
+                entry_point=ENTRY_POINT,
                 parameters=[
                     tasks.SilverTaxiAggTask.get_class_name(),
                     "--catalog",
@@ -65,11 +63,11 @@ py_dbx_example_job = Job(
                 ],
             ),
             depends_on=[{"task_key": "bronze_taxi"}, {"task_key": "bronze_accuweather"}],
-            run_if="ALL_SUCCESS",
-            environment_key="default",
+            run_if=RunIf.ALL_SUCCESS,
+            environment_key=DEFAULT_ENVIRONMENT.environment_key,
         ),
     ],
-    environments=[default_environment],
+    environments=[DEFAULT_ENVIRONMENT],
 )
 
 
